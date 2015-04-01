626b898ba17c6862f57cf0174b6e3a8479f79390
kaltirim <kaltirim@lab.grid.auth.gr>
2015-04-01 16:23:45 +0300

    match algorithm fixes and final steps for local run needs modifications to run for hadoop

--- match-files.py
+++ match-files.py
@@ -13,9 +13,9 @@ def main(argv):
    filename2 = ''
    # try-loop to assign the given arguments to the variables
    try:
-      opts, args = getopt.getopt(argv,"ho:",["file1=","file2=","ofile="])
+      opts, args = getopt.getopt(argv,"ho:",["file1=","file2=","output="])
    except getopt.GetoptError:
-      print 'usage: match-files.py --file1=<first_file> --file2=<second_file> -o <outputfile>'
+      print 'usage: match-files.py --file1=<first_file> --file2=<second_file> --ofile=<outputfile>'
       sys.exit(2)
    for opt, arg in opts:
       if opt in ("--file1"):
@@ -28,38 +28,47 @@ def main(argv):
    print 'Second file is', filename2
    print 'Output file is ', outputfile
    #get confirmation that the given arguments are right to continue
-   str = raw_input("enter c to continue: ")
+   answer = raw_input("enter c to continue: ")
 
-   if str=="c":
+   if answer=="c":
         #open files
         file1 = open(filename1,"r")
         file2 = open(filename2, "r")
-       # outfile = codecs.open(outputfile, "w")
+        #outfile = open(outputfile, "w")
         
         #read fields of file1
         fields1 = file1.readline().strip().split(reg_ex)
         #read fields of file 2
         fields2 = file2.readline().strip().split(reg_ex)
-        print fields1
-        print fields2
         #read every line of file2 into one variable
         file2_lines = file2.readlines()
         #parse every line of file1
-       # for line in file1:
-       #     i, j = 0
+        num1 = 2
+        for line in file1:
             #split the lines into parts
-       #     parts1 = line.split(reg_ex)
-       #     parts2 = file2_lines.split(reg_ex)
-       #     for part in parts1:
-       #         if fields1[i] == fields2[j]:
-                    ### match the fields between the lines
-       #         else:
-       #             i = i + 1
-       #             j = j + 1
+            parts1 = line.strip().split(reg_ex)
+            #parse every line of file2
+            num2 = 2
+            for line2 in file2_lines:
+              output = ''
+              parts2 = line2.strip().split(reg_ex)
+              for part1 in parts1:
+                for part2 in parts2:
+                  words = part2.strip().split(' ')
+                  for word in words:
+                    if word in part1.split(' '):
+                      output = output + word + ' '
+              output = re.sub(r'\W[^A-Za-z0-9|]\W', "", output)
+              if output != '':
+                output = output + ' | line_in_file_1: ' + str(num1) + ' | line_in_file_2: ' + str(num2) + '\n'
+                print output
+                #outfile.write(output)
+              num2 = num2 + 1
+            num1 = num1 + 1
         #close files
         file1.close()
         file2.close()
-       # outfile.close()
+        #outfile.close()
    else:
         exit(0)
 
