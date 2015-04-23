815e1164ce90144ba330296edc5d3c13e724f7ce
kaltirim <kaltirim@lab.grid.auth.gr>
2015-04-23 17:06:11 +0300

    progress

--- run.sh
+++ run.sh
@@ -3,8 +3,9 @@
 #-file 'home/efi/Projects/File-Parsing/test.txt' \
 
 hadoop jar $HADOOP_PREFIX/hadoop-streaming.jar \
--file '/home/efi/Projects/File-Parsing/mapper2.py' \
--mapper '/home/efi/Projects/File-Parsing/mapper2.py test.txt' \
+-file '/home/efi/Projects/File-Parsing/wordcount-mapper.py' \
+-mapper '/home/efi/Projects/File-Parsing/wordcount-mapper.py french.txt' \
 -file '/home/efi/Projects/File-Parsing/reducer.py'  \
 -reducer '/home/efi/Projects/File-Parsing/reducer.py' \
+-file '/home/efi/Projects/File-Parsing/french.txt'  \
 -input /user/efi/input -output /user/efi/output
--- wordcount-mapper.py
+++ wordcount-mapper.py
@@ -2,6 +2,13 @@
 
 import sys
 
+file2 = open(sys.argv[1], "r")
+
+for line in file2:
+    line = line.strip()
+    words = line.split()
+    for word in words:
+        print '%s\t%s' % (word, 1)
 # input comes from STDIN (standard input)
 for line in sys.stdin:
     # remove leading and trailing whitespace
@@ -10,9 +17,10 @@ for line in sys.stdin:
     words = line.split()
     # increase counters
     for word in words:
+        pass
         # write the results to STDOUT (standard output);
         # what we output here will be the input for the
         # Reduce step, i.e. the input for reducer.py
         #
         # tab-delimited; the trivial word count is 1
-        print '%s\t%s' % (word, 1)
+        ##print '%s\t%s' % (word, 1)
